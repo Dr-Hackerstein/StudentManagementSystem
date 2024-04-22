00b481b3cb85884be42ef5e06cf09ebabcd80724
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-22 16:01:52 -0600

    GET METHODS
    Added Get Methods for retrieving student list and also a Post Method to be able to save a new student.

--- student-management-system/src/main/java/com/kevin/sms/studentmanagementsystem/StudentManagementSystemApplication.java
+++ student-management-system/src/main/java/com/kevin/sms/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,29 @@
 package com.kevin.sms.studentmanagementsystem;
 
+import com.kevin.sms.studentmanagementsystem.entity.Student;
+import com.kevin.sms.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception {
+//		Student student1 = new Student("Alexia", "Otero", "alexiamorales11109@gmail.com");
+//		Student student2 = new Student("Liliana", "Morales", "liliana@gmail.com");
+//		Student student3 = new Student("Maura", "Morales", "maura@gmail.com");
+//
+//		studentRepository.save(student1);
+//		studentRepository.save(student2);
+//		studentRepository.save(student3);
+	}
 }
